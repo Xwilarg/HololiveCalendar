48f16817a613e33e938e8f430f00b8d4bb849bee
Xwilarg <xwilarg@yahoo.fr>
2021-01-13 19:41:11 +0100

    Add calendar generation

---
+++ js/calendar.js
@@ -0,0 +1,37 @@
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+function getNewMonthHtml(date) {
+    return '<div class="month"><ul><li>' + monthNames[date.getMonth()] + '<br><span class="headerYear">' + date.getFullYear() + '</span></li></ul></div>' +
+    '<ul class="weekdays"><li>Mo</li><li>Tu</li><li>Mo</li><li>We</li><li>Fr</li><li>Sa</li><li>Su</li></ul><ul class="days">';
+}
+
+function getEndMonthHtml() {
+    return '</ul></div>';
+}
+
+function initCalendar() {
+    let date = new Date("2017-01-01");
+    let current = new Date(); // Today
+    let str = ""; // Final HTML
+
+    while (date < current) {
+        str += getNewMonthHtml(date);
+
+        // Increase days
+        let currentMonth = date.getMonth();
+        let i = 0;
+        do {
+            str += '<li>' + (date.getDate()) + '</li>';
+            date.setDate(date.getDate() + 1);
+        } while (date.getMonth() === currentMonth);
+
+        str += getEndMonthHtml();
+    }
+    
+    document.getElementById("calendar").innerHTML = str;
+}
+
+initCalendar();
