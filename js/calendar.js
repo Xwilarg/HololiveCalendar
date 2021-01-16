df1dad85fe55c1b2a3ea2c42993eb7dbc80f86f1
Xwilarg <xwilarg@yahoo.fr>
2021-01-16 18:21:52 +0100

    Add popups

--- js/calendar.js
+++ js/calendar.js
@@ -16,6 +16,15 @@ function compareDates(date1, date2) {
     return date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth() && date1.getFullYear() === date2.getFullYear();
 }
 
+function setPopup(elemId, className) {
+    let tooltip = document.getElementById('tooltip');
+    let elem = document.getElementById(elemId);
+    tooltip.classList = [ className ];
+    Popper.createPopper(elem, tooltip, {
+        placement: 'right',
+    });
+}
+
 function initCalendar() {
     let date = new Date(Date.UTC(2017, 0, 1));
     let current = new Date(); // Today
@@ -41,10 +50,17 @@ function initCalendar() {
         do {
             let currMembers = members.find((e) => compareDates(e.debutDate, date));
             let className = "";
+            let genName = "";
+            let onClick = "";
+            let elemId = "";
             if (currMembers !== undefined) {
-                className = "selected " + currMembers.genID[0];
+                genName = currMembers.genID[0];
+                className = "selected " + genName;
+                elemId = "x" + date.getFullYear() + "x"+  date.getMonth() + "x" + date.getDate();
+                onClick = "setPopup(\'" + elemId + "\', \'" + genName + "\')";
             }
-            str += '<li class="data ' + className + '">' + (date.getDate()) + '</li>';
+            str += '<li class="data ' + className + '" id="' + elemId + '" onclick="' + onClick + '">'
+            + (date.getDate()) + '</li>';
             date.setDate(date.getDate() + 1);
         } while (date.getMonth() === currentMonth);
 
